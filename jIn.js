6aa6f146a90246d015a32c7b1d97157e1c1abc69
jerryxu <13048699388@163.com>
2019-07-19 17:27:55 +0800

    调整热开发配置

--- jIn.js
+++ jIn.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 const server = require('./lib/server');
 require('yargs')
-  .command('serve [port] [configPath]','启动本地开发服务',(yargs)=>{
+  // .command('$0', 'the default command', () => {}, (argv) => {
+  //   console.log('this command will be run by default')
+  // })
+  .command(`start <configPath> [port]`,'启动本地开发服务',(yargs)=>{
     yargs.positional('port',{
       describe:'服务占用端口',
       default:5000
@@ -10,9 +13,15 @@ require('yargs')
     if(argv.verbose) console.log(`服务启动，端口：${argv.port}`)
     server.start(argv.port,argv.configPath);
   })
-  .option('verbose', {
-    alias: 'v',
-    default: false
-  })
+  // .option('channel',{
+  //   alias:'ch',
+  //   describe: '选择入口渠道',
+  // })
+  // .option('verbose', {
+  //   alias: 'v',
+  //   default: false
+  // })
+  // .choices('ch',['kylinwechat','kylintouch'])
+  .help('help')
   .argv;
 
